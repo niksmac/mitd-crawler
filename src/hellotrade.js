d14099de925402edc725a5e2ea50a48c3756e7bc
salusn <salu.s@lightrains.com>
2017-01-16 11:45:17 +0530

    Completed hellotrade.com

--- src/hellotrade.js
+++ src/hellotrade.js
@@ -3,7 +3,6 @@ var Crawler = require("crawler");
 var url = require('url');
 var fs = require('fs');
 var u = require('../utils.js');
-require('locus');
 
 var c = new Crawler({
     maxConnections : 1,
@@ -17,21 +16,20 @@ var c = new Crawler({
             $('div.wd_sm div.ten_sm_box').each(function() {
                 var $this = $(this);                
                 var summary = $this.find('p').html().trim().split('<br>');                
-                var assas  = u.urlify(summary[0]).split('"');
-                var sum1 = $this.find('div.tendr_main div:nth-child(1) div.tendr_right div.tendr_left').text().trim();
-               //eval(locus);
+                var assas  = u.urlify(summary[0]).split('"');             
+                
             if($this.find('div.tendr_cal div:nth-child(2) p').text().trim() != '') {
                 data.push({
                     'name'          : $this.find('div.tendr_cal div:nth-child(2) p').text().trim(),
-                   // 'description'   : "NA",
-                   // 'date'          : new Date($this.find('td:nth-child(2)').text().trim()).getTime()/1000,
+                    'description'   : $this.find('div.tendr_main div.tendr_right div:nth-child(1) p.tendr_md').text().trim(),
+                    'date'          : "NA",
                     'closingDate'   : new Date($this.find('div.tendr_cal div:nth-child(1) p.tendr_right').text().trim()).getTime()/1000,
                     'link'          : "http://tenders.hellotrade.com/tenders.php?pid=33676653"+assas,
-                    // 'amount'        : {
-                    //     'currency'     : "INR",
-                    //     'value'        : summary[1].replace(/[^\d\.]/g, ''),
-                    // },
-                    'category'      : [],
+                    'amount'        : {
+                        'currency'     : "USD",
+                        'value'        : $this.find('div.tendr_main div.ht_100').text().replace(/[^\d\.]/g, ''),
+                    },
+                    'category'      : [$this.find('div.tendr_main div.tendr_right div:nth-child(2) p.tendr_md').text().trim()],
                     'tenderNumber' : "NA"
 
                 });
