7ed5ca83a0a0769668cc13d8373bbc393db41296
salusn <salu.s@lightrains.com>
2017-01-17 13:21:40 +0530

    Completed doitmaryland.gov

---
+++ src/doitmaryland.js
@@ -0,0 +1,59 @@
+var cheerio = require("cheerio");
+var Crawler = require("crawler");
+var url = require('url');
+var fs = require('fs');
+var u = require('../utils.js');
+require('locus');
+
+var c = new Crawler({
+    maxConnections : 1,
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        } else {
+            $ = cheerio.load(res.body);
+            var title = "";
+            var data = [];
+            $('div#ctl00_PlaceHolderMain_ctl02__ControlWrapper_RichHtmlField ul li').each(function() {
+
+                var $this = $(this);                                             
+                var sum = $this.html().trim().split('<br>');
+                var link  = u.urlify(sum[0]).split('"');
+                link = link[1];                
+                var data_proposal = $this.text().split(':');               
+                title = data_proposal[1];
+                var due  = data_proposal[0].trim().split(' ')[1].trim().split('/');
+                var date = new Date(due[2],due[0]-1,due[1]).getTime()/1000;  
+              
+                if(typeof(title) != "undefined") {
+                    
+                    data.push({
+                    'name'          : title.trim(),
+                    'description'   : "NA",
+                    'date'          : "NA",
+                    'closingDate'   : date,
+                    'link'          : "http://doit.maryland.gov/contracts/Pages/bids.aspx"+link,
+                     'amount'        : {
+                         'currency'     : "NA",
+                         'value'        : "NA",
+                     },
+                    'category'      : [],
+                    'tenderNumber' : "NA"
+
+                    });
+                }
+            })
+            console.log(data)
+        }
+        done();
+    }
+});
+
+
+// Queue just one URL, with default callback 
+ c.queue('http://doit.maryland.gov/contracts/Pages/bids.aspx');
+
+module.exports.getData = c.length;
+
+
