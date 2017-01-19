582d62543bc1507592d5c312f77ee5390d75dfc6
salusn <salu.s@lightrains.com>
2017-01-19 14:19:18 +0530

    completed procure.ohio.gov

--- src/procureohio.js
+++ src/procureohio.js
@@ -14,9 +14,33 @@ var c = new Crawler({
             $ = cheerio.load(res.body);
             var title = "";
             var data = [];
-           // $('').each(function() {            
-                
-           // })
+            $('div#full table.results tr').each(function() { 
+                var $this = $(this);
+                var date  = $this.find('td:nth-child(8)').text().trim().split('/');
+                var start_date = new Date(date[2],date[0]-1,date[1]).getTime()/1000;
+                var end_date = $this.find('td:nth-child(9)').text().trim().split('/');
+                var close_date = new Date(end_date[2],end_date[0]-1,end_date[1]).getTime()/1000;
+
+                if($this.find('td:nth-child(7)').html() != null) {
+                var sum = $this.find('td:nth-child(7)').html().trim().split('<br>');
+                var link  = u.urlify(sum[0]).split('"');
+                link = link[1]
+                }
+
+                if($this.find('td:nth-child(1)').text().trim() != '') {
+                    data.push({
+                        'name'          : $this.find('td:nth-child(1)').text().trim(),
+                        'description'   : "",
+                        'date'          : start_date,
+                        'closingDate'   : close_date,
+                        'link'          : "https://procure.ohio.gov"+link,                  
+                        'currency'     : "NA",
+                        'value'        : "NA",                   
+                        'category'      : [$this.find('td:nth-child(2)').text().trim()],
+                        'tenderNumber' : $this.find('td:nth-child(7)').text().trim()
+                    });           
+                }    
+            })
             console.log(data)
         }
         done();
