ab9f025c1ad0324ccb2eb1d0c93611a085d8867c
salusn <salu.s@lightrains.com>
2017-07-26 17:34:52 +0530

    Added gulp merger

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,14 @@ var merge = require('gulp-merge-json');
 
 gulp.task('merger', function() {
 	gulp.src('data/*.json')
-    	.pipe(merge())
+    	.pipe(merge({
+    		startObj:[],
+    		concatArrays:true,
+    		mergeArrays:false
+    	}))
     	.pipe(gulp.dest('./dist'));
 })
 
 gulp.task('default', ['merger']);
+
+
