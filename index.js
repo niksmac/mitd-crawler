616566324860207afc6c72a23c0d92fd3fbbbe4c
niksmac <hello@nikhilmohan.in>
2017-07-27 07:35:08 +0530

    Removed unused code

--- index.js
+++ index.js
@@ -9,17 +9,8 @@ var Crawler = require('crawler'),
   moment = require('moment')
 fs = require('fs')
 
-var i = 1, c = 0
-
-var table = new Table({
-  head: ['Sl.', 'Domain', 'How many', 'When'],
-  colWidths: [6, 30, 10, 17]
-})
-var date = new Date()
-
 glob.sync('./src/**/*.js').forEach(function (file) {
   var name = file.replace('.js', '')
   var mname = name.replace('./src/', '')
   var xmname = require('./' + file)
 })
-
