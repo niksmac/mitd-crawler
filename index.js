86369fda3a9f41d36ddb217c05a7fcdfd372c952
niksmac <hello@nikhilmohan.in>
2017-07-27 08:08:26 +0530

    Added log format

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-global.config = require('konfig')()
+var d = new Date()
+console.log('====> ' + d.toLocaleString())
 
 var Crawler = require('crawler'),
   url = require('url'),
   glob = require('glob'),
   path = require('path'),
-  colors = require('colors'),
-  Table = require('cli-table2'),
-  moment = require('moment')
-fs = require('fs')
+  fs = require('fs')
 
 glob.sync('./src/**/*.js').forEach(function (file) {
-  var name = file.replace('.js', '')
-  var mname = name.replace('./src/', '')
-  var xmname = require('./' + file)
+  require('./' + file)
 })
+
+process.on('exit', function () { process.exit(1) })
