8a784ef48ca7282affd2a64d1714bd3971c8ee57
niksmac <hello@nikhilmohan.in>
2017-07-27 08:08:45 +0530

    Added log

--- writer.js
+++ writer.js
@@ -1,11 +1,16 @@
 'use strict'
+var fs = require('fs')
 
 var jsonfile = require('jsonfile')
 exports.writeJson = function (jsons, scriptName) {
-  var file = process.cwd() + '/data/'+scriptName+'.json'
-  var newfile = file.replace('.js', '');
-  var fd = fs.openSync(newfile, 'w');
+  var file = process.cwd() + '/data/' + scriptName + '.json'
+  var newfile = file.replace('.js', '')
+  console.log('Writing ' + jsons.length + ' values ' + newfile.replace(process.cwd(), ''))
+  var fd = fs.openSync(newfile, 'w')
+
   jsonfile.writeFile(newfile, jsons, {spaces: 2}, function (err) {
-    console.error(err)
+    if (err) {
+      throw new Error('Failed to write ' + newfile)
+    }
   })
 }
