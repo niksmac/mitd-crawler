6a58d157203053f716132663e409ba335f3a7c01
niksmac <hello@nikhilmohan.in>
2017-07-27 08:07:22 +0530

    Added copy gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
-var gulp = require('gulp');
-var merge = require('gulp-merge-json');
+var gulp = require('gulp')
+var merge = require('gulp-merge-json')
+var gulpCopy = require('gulp-copy')
+const SOURCE_FILES = ['dist/*']
+const DESTINATION = './dist/combined/'
 
-gulp.task('merger', function() {
-	gulp.src('data/*.json')
+gulp.task('merger', function () {
+  gulp.src('data/*.json')
     	.pipe(merge({
-    		startObj:[],
-    		concatArrays:true,
-    		mergeArrays:false
+    		startObj: [],
+    		concatArrays: true,
+    		mergeArrays: false
     	}))
-    	.pipe(gulp.dest('./dist'));
+    	.pipe(gulp.dest('./dist'))
 })
 
-gulp.task('default', ['merger']);
+gulp.task('copy', function () {
+  gulp.src(SOURCE_FILES)
+    .pipe(gulp.dest(DESTINATION))
+})
 
-
+gulp.task('default', ['merger', 'copy'])
