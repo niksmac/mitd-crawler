f1eb9f18f98b9f3e444caf0523d9c9330c8c8be1
salusn <salu.s@lightrains.com>
2017-08-10 16:32:43 +0530

    Hashing of link and added as uuid

--- writer.js
+++ writer.js
@@ -1,8 +1,14 @@
 'use strict'
 var fs = require('fs')
+var hash = require('object-hash');
 
 var jsonfile = require('jsonfile')
 exports.writeJson = function (jsons, scriptName) {
+
+jsons.forEach(function(value){  
+  value.uuid = hash(value.link)
+});	
+console.log(jsons);
   var file = process.cwd() + '/data/' + scriptName + '.json'
   var newfile = file.replace('.js', '')
   console.log('Writing ' + jsons.length + ' values ' + newfile.replace(process.cwd(), ''))
